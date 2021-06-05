245d3ab5d1c4a6957b435b7c2bb4502486469a87
cullen <cullen.dolan@gmail.com>
2021-06-05 11:55:12 -0500

    working to get the rds role attached

--- iam.tf
+++ iam.tf
@@ -70,17 +70,46 @@ resource "aws_iam_policy" "RDSAccessS3wTF"{
           "arn:aws:s3:::cullenbucketfordb/*",
           "arn:aws:s3:::lscdatalake-dev/*"
         ]
-      }
+      },
     ]
   })
+  tags = {
+    Environment = "TF-Dev"
+  }
 }
 
 # rds role
-
+resource "aws_iam_role" "RDSImportS3roleTF"{
+  name    = "RDSImportS3roleTF"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 # attach role to instance
+resource "aws_iam_policy_attachment" "AttachPolicytoRDS" {
+  name  = "AttachPolicytoRDS"
+  roles = [aws_iam_role.RDSImportS3roleTF.name]
+  policy_arn = aws_iam_policy.RDSAccessS3wTF.arn
+}
 
-
-
+// # resource association
+// resource "aws_db_instance_role_association" "AssociateRolewRDSTF" {
+//   db_instance_identifier = aws_db_instance.lscdevdbinstance.id
+//   feature_name           = "s3Import"
+//   role_arn               = aws_iam_role.RDSImportS3roleTF.arn
+// }
 
 // # add this once keybase is figured out
 // resource "aws_iam_user_login_profile" "DataLoadUserLogin" {
